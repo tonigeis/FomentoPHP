715c49e9f40ab56547497ad0b562eaca4bfdf5de
tonigeis <antoniogeisinfiesta@gmail.com>
2016-09-13 20:39:54 +0200

    First Commit

---
+++ arraymultidimensional_cars.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars = array
+  (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+  );
+    
+for ($row = 0; $row < count($cars); $row++) {
+  if ($cars[$row][1] < 20) {
+    echo "<p><b>Row number $row</b></p>";
+    echo "<ul>";
+    for ($col = 0; $col < count($cars[$row]); $col++) {
+      echo "<li>".$cars[$row][$col]."</li>";
+    }
+    echo "</ul>";
+  }
+}
+?>
+
+</body>
+</html>
