fb5c1147e99b6a81a215f8d2c8b15aa1feb30e99
tonigeis <antoniogeisinfiesta@gmail.com>
2016-09-13 19:35:01 +0200

    Añadimos condición
    Condición que sea mayor que 5 y múltiplo de 5

--- numerosimpares.php
+++ numerosimpares.php
@@ -4,9 +4,12 @@
 
 <?php  
 $numbers = array(1, 2, 3, 4, 7, 12, 13, 17, 18, 19); 
+$numbers = array(1, 2, 3, 4, 7, 12, 13, 15, 17, 18, 19); 
+
 
 foreach ($numbers as $num) {
 	if ($num % 2 == 1) {
+	if ($num % 2 == 1 && $num > 5 && $num % 5 == 0) {
 		echo "$num <br>";
 	}
 }
